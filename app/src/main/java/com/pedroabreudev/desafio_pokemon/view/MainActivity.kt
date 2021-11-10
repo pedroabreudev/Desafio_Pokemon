952ee0b8a615ca6e1392befbe108287e1282955e
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-09 22:48:54 -0400

    Desafio Finalizado.

--- app/src/main/java/com/pedroabreudev/desafio_pokemon/view/MainActivity.kt
+++ app/src/main/java/com/pedroabreudev/desafio_pokemon/view/MainActivity.kt
@@ -1,28 +1,37 @@
 package com.pedroabreudev.desafio_pokemon.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pedroabreudev.desafio_pokemon.R
 import com.pedroabreudev.desafio_pokemon.domain.Pokemon
+import com.pedroabreudev.desafio_pokemon.viewmodel.PokemonViewModel
+import com.pedroabreudev.desafio_pokemon.viewmodel.PokemonViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val recyclerView by lazy {
+        findViewById<RecyclerView>(R.id.pokemon_recyclerView)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.pokemon_recyclerView)
+        viewModel.pokemons.observe(this, Observer {
+            loadRecyclerView(it)
+        })
+    }
 
-        val pikachu = Pokemon(
-            25,
-            "Pikachu"
-        )
-
-        val pokemons = listOf(pikachu, pikachu, pikachu, pikachu, pikachu, pikachu)
-
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+    private fun loadRecyclerView(pokemons: List<Pokemon?>) {
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
